3be8a98bc218b147f7b7b8b468085856b3fbb72b
Riza Fahmi <rizafahmi@gmail.com>
2016-10-21 10:48:41 +0700

    First Element

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+class Leaderboard extends React.Component {
+  render() {
+
+    return (
+      <div>
+        <h1>Leaderboard</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render( <Leaderboard />, document.getElementById('root'));
