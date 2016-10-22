e6670ed632acef6d11febdbb671358c4ea03549a
Riza Fahmi <rizafahmi@gmail.com>
2016-10-22 07:53:49 +0700

    Refactor Leaderboard

---
+++ src/Leaderboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Leaderboard extends React.Component {
+  render() {
+
+    return (
+      <div>
+        <h1>Leaderboard</h1>
+      </div>
+    )
+  }
+}
+
+export default Leaderboard
--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Leaderboard extends React.Component {
-  render() {
+import Leaderboard from './Leaderboard.js'
 
-    return (
-      <div>
-        <h1>Leaderboard</h1>
-      </div>
-    )
-  }
-}
 
 ReactDOM.render( <Leaderboard />, document.getElementById('root'));
