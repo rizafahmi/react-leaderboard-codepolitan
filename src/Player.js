f8fecdc161f1d245a23fe1c1fd0c58d8463c8449
Riza Fahmi <rizafahmi@gmail.com>
2016-10-22 11:54:50 +0700

    Finish increment and decrement features

--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,25 @@
 import React from 'react'
 
 class Player extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      score: this.props.score
+    }
+
+    this.incrementScore = this.incrementScore.bind(this)
+    this.decrementScore = this.decrementScore.bind(this)
+  }
+  incrementScore(event) {
+    this.setState({
+      score: this.state.score + 10
+    })
+  }
+  decrementScore(event) {
+    this.setState({
+      score: this.state.score - 10
+    })
+  }
   render() {
     const playerStyle = {
       display: 'flex',
@@ -25,9 +44,9 @@ class Player extends React.Component {
         </div>
         <div style={playerScoreStyle}>
           <div className="counter">
-            <button style={buttonStyle}> - </button>
-            <span style={scoreStyle}> {this.props.score} </span>
-            <button style={buttonStyle}> + </button>
+            <button style={buttonStyle} onClick={this.decrementScore}> - </button>
+            <span style={scoreStyle}> {this.state.score} </span>
+            <button style={buttonStyle} onClick={this.incrementScore}> + </button>
           </div>
         </div>
       </div>
